c759d7bbe5a6fde79f199d3b931ce896d5424281
Andres <andresjpulido@gmail.com>
2024-09-13 23:22:46 +1200

    seo ajusting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from "astro/config";
-
+import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({ applyBaseStyles: false })],
+  integrations: [tailwind({ applyBaseStyles: false }), sitemap({
+    filter: (page) =>
+      page !== "https://andresjpulido.github.io/palette/" 
+  })],
   i18n: {
     defaultLocale: "es",
     locales: ["en", "es"],
@@ -12,6 +15,6 @@ export default defineConfig({
       prefixDefaultLocale: false,
     },
   },
-  site: 'https://andresjpulido.github.io',
+  site: 'https://industriasmarysol.com',
   base: '/',
 });
